38581f1939a3e022695a0c1b5fd39a24a3884de2
Wesley-robles <robleswesley@gmail.com>
2025-06-22 20:45:15 -0300

    Fix: Resolve conflitos de migração e aplica schema inicial do banco

--- config/settings.py
+++ config/settings.py
@@ -121,3 +121,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Define o nosso User personalizado como o modelo de autenticação padrão
+AUTH_USER_MODEL = 'api.User'
