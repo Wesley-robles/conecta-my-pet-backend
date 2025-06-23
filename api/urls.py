dc80b66426536b0d51fe696a631c1419c1bb0811
Wesley-robles <robleswesley@gmail.com>
2025-06-23 14:39:10 -0300

    Refactor: Usa ViewSet e Router para endpoints de PetShop

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,15 @@
 # api/urls.py
-from django.urls import path
-from .views import PetShopListView
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import PetShopViewSet
 
+# 1. Cria um router
+router = DefaultRouter()
+
+# 2. Registra nosso ViewSet com o router, definindo o prefixo da URL como 'petshops'
+router.register(r'petshops', PetShopViewSet, basename='petshop')
+
+# 3. As URLs da API são agora determinadas automaticamente pelo router.
 urlpatterns = [
-    path('petshops/', PetShopListView.as_view(), name='petshop-list'),
+    path('', include(router.urls)),
 ]
--- api/views.py
+++ api/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import viewsets
 from .models import PetShop
 from .serializers import PetShopSerializer
 
-class PetShopListView(generics.ListAPIView):
+class PetShopViewSet(viewsets.ModelViewSet):
+    """
+    Este ViewSet fornece automaticamente as ações `list`, `retrieve`,
+    `create`, `update` e `destroy` para o modelo PetShop.
+    """
     queryset = PetShop.objects.all()
     serializer_class = PetShopSerializer
